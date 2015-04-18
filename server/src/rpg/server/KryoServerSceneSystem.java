874d8c89344ffab1eeea7d2983338316bd6def3c
Ronald Kinard <furyhunter600@gmail.com>
2015-04-18 01:53:35 -0500

    Rework node existence synchronization.

--- server/src/rpg/server/KryoServerSceneSystem.java
+++ server/src/rpg/server/KryoServerSceneSystem.java
@@ -11,6 +11,7 @@ import rpg.scene.systems.AbstractSceneSystem;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class KryoServerSceneSystem extends AbstractSceneSystem {
 
@@ -20,9 +21,7 @@ public class KryoServerSceneSystem extends AbstractSceneSystem {
     private Map<Integer, Node> nodeMap = new TreeMap<>();
     private Map<Integer, Component> componentMap = new TreeMap<>();
 
-    private List<Node> nodesToAttach = new ArrayList<>();
-    private List<Node> nodesToDetach = new ArrayList<>();
-    private List<Node> nodesToReattach = new ArrayList<>();
+    private Set<Node> nodesToReattach = new TreeSet<>(Comparator.comparingInt(Node::getNetworkID));
 
     private List<Component> componentsToAttach = new ArrayList<>();
     private List<Component> componentsToDetach = new ArrayList<>();
@@ -31,9 +30,9 @@ public class KryoServerSceneSystem extends AbstractSceneSystem {
     private RelevantSetDecider relevantSetDecider = new AlwaysRelevantDecider();
 
     private List<Player> players = new ArrayList<>();
-    private Map<Connection, Player> connectionPlayerMap;
+    private Map<Connection, Player> connectionPlayerMap = new TreeMap<>(Comparator.comparingInt(Connection::getID));
 
-    private Map<Player, Set<Node>> oldRelevantSets;
+    private Map<Player, Set<Node>> oldRelevantSets = new TreeMap<>(Comparator.comparingInt(p -> p.kryoConnection.getID()));
 
     private float timeBuffer = 0;
     private float replicationRate = 10;
@@ -57,7 +56,10 @@ public class KryoServerSceneSystem extends AbstractSceneSystem {
 
         @Override
         public void disconnected(Connection connection) {
-            players.remove(connectionPlayerMap.remove(connection));
+            Player p = connectionPlayerMap.remove(connection);
+            players.remove(p);
+            oldRelevantSets.remove(p);
+
         }
     }
 
@@ -66,26 +68,11 @@ public class KryoServerSceneSystem extends AbstractSceneSystem {
         KryoClassRegisterUtil.registerAll(server.getKryo()); // register all known classes for serialization
         server.addListener(listener);
         server.bind(12523, 12524);
-
-        connectionPlayerMap = new TreeMap<>(Comparator.comparingInt(Connection::getID));
-
-        oldRelevantSets = new TreeMap<>(Comparator.comparingInt(p -> p.kryoConnection.getID()));
     }
 
     @Override
     public void processNode(Node n, float deltaTime) {
 
-    }
-
-    @Override
-    public void nodeAttached(Node n) {
-        nodeMap.put(n.getNetworkID(), n);
-        nodesToAttach.add(n);
-    }
-
-    @Override
-    public void nodeDetached(Node n) {
-        nodesToDetach.add(n);
     }
 
     @Override
@@ -125,27 +112,81 @@ public class KryoServerSceneSystem extends AbstractSceneSystem {
 
         if (timeBuffer > (1f / replicationRate)) {
 
-            players.forEach(p -> {
+            /* During this processing step, the scene is considered "immutable", so we can
+             * process the scene in parallel for all connected clients. Below is the general
+             * process of handling the replication tick for each player:
+             *
+             * 1. Send BeginTick to indicate a block of information for a tick.
+             * 2. Get the new relevant set.
+             * 3. Ensure that each node in the relevant set's parent is in the relevant set.
+             * 4. Compare the old relevant set and new relevant set to get the delta relevance. This yields us 3 sets:
+             *    new relevancy (attach), new irrelevancy (detach), and consistently relevant between ticks.
+             * 4. For consistently relevant nodes, we must figure out if their parent has changed since the last tick.
+             *    If it has, then we have to reattach -- and if the new parent is not relevant, then that node can't
+             *    be relevant, so we'll go ahead and send a detach event for that node.
+             * 5. Replicate components.
+             * 6. EndTick
+             */
+            players.parallelStream().forEach(p -> {
+                // Send begin tick.
                 BeginTick bt = new BeginTick();
                 bt.tickID = currentTick;
                 p.kryoConnection.sendTCP(bt);
 
-                // New relevant set
+                // Get the relevant set
                 Set<Node> newRelevantSet = relevantSetDecider.getRelevantSetForNode(getParent(), p.possessedNode);
                 Set<Node> oldRelevantSet = oldRelevantSets.get(p);
 
-                // All nodes which were not in oldRelevantSet and are in newRelevantSet
-                Set<Node> newlyRelevant = new TreeSet<>(newRelevantSet);
+                // Remove nodes whose parents aren't relevant from relevant set.
+                List<Node> notActuallyRelevant = newRelevantSet.parallelStream()
+                        .filter(n -> !newRelevantSet.contains(n.getParent()))
+                        .collect(Collectors.toList());
+                newRelevantSet.removeAll(notActuallyRelevant);
+
+                // The relevant.
+                Set<Node> newlyRelevant = new TreeSet<>(Comparator.comparingInt(Node::getNetworkID));
+                newlyRelevant.addAll(newRelevantSet);
                 newlyRelevant.removeAll(oldRelevantSet);
 
-                // All nodes which were not in newRelevantSet and are in oldRelevantSet
-                Set<Node> newlyNonRelevant = new TreeSet<>(oldRelevantSet);
+                // The no longer relevant.
+                Set<Node> newlyNonRelevant = new TreeSet<>(Comparator.comparingInt(Node::getNetworkID));
+                newlyNonRelevant.addAll(oldRelevantSet);
                 newlyNonRelevant.removeAll(newRelevantSet);
+
+                // The consistently relevant nodes between ticks.
+                Set<Node> inBoth = new TreeSet<>(Comparator.comparingInt(Node::getNetworkID));
+                inBoth.addAll(newRelevantSet);
+                inBoth.retainAll(oldRelevantSet);
+
+                // Ensure that any reattached nodes get reattach messages IF AND ONLY IF their new parent is relevant.
+                notActuallyRelevant = inBoth.parallelStream()
+                        .filter(nodesToReattach::contains)
+                        .filter(n -> !newRelevantSet.contains(n))
+                        .collect(Collectors.toList());
+
+                newlyNonRelevant.addAll(notActuallyRelevant);
+                inBoth.removeAll(notActuallyRelevant);
+
+                // Nodes to send reattachment messages are all nodes consistently relevant that are also in the reattachment set.
+                List<Node> nodesForReattachment = inBoth.parallelStream()
+                        .filter(nodesToReattach::contains)
+                        .collect(Collectors.toList());
+
+                // Reattach nodes that need to be sent reattachments.
+                nodesForReattachment.forEach(n -> {
+                    if (n.getNetworkID() > 0) {
+                        NodeReattach nodeReattach = new NodeReattach();
+                        nodeReattach.nodeID = n.getNetworkID();
+                        nodeReattach.parentID = n.getParent().getNetworkID();
+                        p.kryoConnection.sendTCP(nodeReattach);
+                    }
+                });
+
 
                 // For a player that has yet to receive a tick,
                 // newlyRelevant == newRelevantSet (everything)
                 // newlyIrrelevant == oldRelevantSet (nothing)
-                newRelevantSet.forEach(n -> {
+                newlyRelevant.forEach(n -> {
                     if (n.getNetworkID() > 0) {
                         NodeAttach nodeAttach = new NodeAttach();
                         nodeAttach.nodeID = n.getNetworkID();
@@ -163,15 +204,21 @@ public class KryoServerSceneSystem extends AbstractSceneSystem {
                     }
                 });
 
+                // Now, again for all of the now-relevant nodes, create and replicate all components.
+
                 EndTick endTick = new EndTick();
                 endTick.tickID = currentTick;
 
-                currentTick++;
+                oldRelevantSets.put(p, newRelevantSet);
             });
 
             timeBuffer = 0;
+
+            currentTick++;
             // We set to 0 because we don't want to end up sending
             // a whole bunch of ticks all at once if there's a long GC pause.
+
+            nodesToReattach.clear();
         }
 
 
