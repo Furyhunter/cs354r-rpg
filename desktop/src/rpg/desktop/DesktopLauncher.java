292020899dd87a2a19fa308541369126e944fe75
Ronald Kinard <furyhunter600@gmail.com>
2015-04-19 17:45:19 -0500

    Adjust framerate caps on client.

--- desktop/src/rpg/desktop/DesktopLauncher.java
+++ desktop/src/rpg/desktop/DesktopLauncher.java
@@ -10,6 +10,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.resizable = true;
+		config.foregroundFPS = 300;
+		config.backgroundFPS = 30;
 		App app = new App();
 		Arrays.stream(arg).forEach(app::addRunArgument);
 		new LwjglApplication(app, config);
