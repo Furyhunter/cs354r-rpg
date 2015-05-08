a4db7cbe492b0a25c5987055bd95bb987328414b
Ronald Kinard <furyhunter600@gmail.com>
2015-05-07 19:47:33 -0500

    Cap client FPS again

--- desktop/src/rpg/desktop/DesktopLauncher.java
+++ desktop/src/rpg/desktop/DesktopLauncher.java
@@ -10,8 +10,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.resizable = true;
-        config.foregroundFPS = 0;
-        config.backgroundFPS = 30;
+		config.foregroundFPS = 300;
+		config.backgroundFPS = 30;
 		config.width = 800;
 		config.height = 600;
         config.vSyncEnabled = false;
